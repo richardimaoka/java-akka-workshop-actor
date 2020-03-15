fdcb7b6ede0a191519b31e9502e633b03d83cbf9
Richard Imaoka <richard.e.imaoka@gmail.com>
2020-03-15 19:00:53 +0900

    Rename InventoryActor to TicketStockActor

--- src/main/java/com/mycompany/actor/TicketStockActor.java
+++ src/main/java/com/mycompany/actor/TicketStockActor.java
@@ -3,14 +3,14 @@ package com.mycompany.actor;
 import akka.actor.typed.*;
 import akka.actor.typed.javadsl.*;
 
-public class InventoryActor {
+public class TicketStockActor {
   /********************************************************************************
    *  Actor Behaviors
    *******************************************************************************/
   // public: the only Behavior factory method accessed from outside the actor
   public static Behavior<Message> create(int ticketId, int quantity){
     if(quantity < 0)
-      throw new RuntimeException("Inventory quantity cannot be negative");
+      throw new RuntimeException("TicketStock quantity cannot be negative");
     else
       return Behaviors.setup(context -> behavior(context, ticketId, quantity));
   }
@@ -18,10 +18,10 @@ public class InventoryActor {
   // private: never accessed from outside the actor
   private static Behavior<Message> behavior(ActorContext<Message> context, int ticketId, int quantity) {
     return Behaviors.receive(Message.class)
-      .onMessage(InventoryDecrement.class, message -> {
+      .onMessage(TicketStockDecrement.class, message -> {
         int decrementedQuantity = quantity - message.quantityDecrementedBy;
         if (decrementedQuantity < 0) {
-          message.sender.tell("inventory cannot have a negative quantity");
+          message.sender.tell("TicketStock cannot have a negative quantity");
           return Behaviors.same();
         } else {
           return behavior(context, ticketId, decrementedQuantity);
@@ -35,12 +35,12 @@ public class InventoryActor {
    *******************************************************************************/
   public interface Message {}
 
-  public static final class InventoryDecrement implements Message {
+  public static final class TicketStockDecrement implements Message {
     public final int ticketId;
     public final int quantityDecrementedBy;
     public final ActorRef<String> sender;
 
-    public InventoryDecrement(int ticketId, int quantityDecrementedBy, ActorRef<String> sender) {
+    public TicketStockDecrement(int ticketId, int quantityDecrementedBy, ActorRef<String> sender) {
       this.ticketId = ticketId;
       this.quantityDecrementedBy = quantityDecrementedBy;
       this.sender = sender;
