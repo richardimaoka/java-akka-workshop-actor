c13989b1061c6d8322b95d50d45a79788c934c33
Richard Imaoka <richard.e.imaoka@gmail.com>
2020-03-15 22:53:39 +0900

    Http server

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -4,16 +4,25 @@ import akka.actor.typed.*;
 import akka.http.javadsl.*;
 import akka.stream.*;
 import com.mycompany.actor.*;
+import com.mycompany.route.*;
 
 public class Main {
 
   public static void main(String[] args) throws Exception {
     // boot up server using the route as defined below
     var system = ActorSystem.create(GuardianActor.create(), "userGuardian");
-    var http = Http.get(system.classicSystem());
     var materializer = Materializer.createMaterializer(system);
+
+    var http = Http.get(system.classicSystem());
+    var allRoute = new AllRoute();
+    var routeFlow = allRoute.route().flow(system.classicSystem(), materializer);
+    var binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8080), materializer);
 
     System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
     System.in.read(); // let it run until user presses return
+
+    binding
+      .thenCompose(ServerBinding::unbind)
+      .thenAccept(unbound -> system.terminate());
   }
 }
--- src/main/java/com/mycompany/route/AllRoute.java
+++ src/main/java/com/mycompany/route/AllRoute.java
@@ -3,4 +3,9 @@ package com.mycompany.route;
 import akka.http.javadsl.server.*;
 
 public class AllRoute extends AllDirectives {
+  public AllRoute(){}
+
+  public Route route(){
+    return get(() -> complete("hello world"));
+  }
 }
