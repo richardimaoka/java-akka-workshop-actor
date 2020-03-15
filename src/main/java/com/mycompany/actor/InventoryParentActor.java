2911d9a170d409443d0bdc7801cac5908765105a
Richard Imaoka <richard.e.imaoka@gmail.com>
2020-03-15 20:30:15 +0900

    InventoryParentActor

---
+++ src/main/java/com/mycompany/actor/InventoryParentActor.java
@@ -0,0 +1,49 @@
+package com.mycompany.actor;
+
+import akka.actor.typed.*;
+import akka.actor.typed.javadsl.*;
+
+import java.util.*;
+
+public class InventoryParentActor {
+  /********************************************************************************
+   *  Actor Behaviors
+   *******************************************************************************/
+  // public: the only Behavior factory method accessed from outside the actor
+  public static Behavior<Message> create(int ticketId, int quantity){
+    Map<String, ActorRef<TicketStockActor.Message>> children = Collections.emptyMap();
+    return Behaviors.setup(context -> behavior(context, children));
+  }
+
+  // private: never accessed from outside the actor
+  private static Behavior<Message> behavior(
+    ActorContext<Message> context,
+    Map<String, ActorRef<TicketStockActor.Message>> children) {
+    return Behaviors.receive(Message.class)
+      .onMessage(CreateTicketStock.class, message -> {
+        var id = UUID.randomUUID();
+        var child = context.spawn(TicketStockActor.create(message.ticketId, message.quantity), id.toString());
+        children.put(id.toString(), child);
+        return behavior(context, children);
+      })
+      .build();
+  }
+
+  /********************************************************************************
+   *  Actor Messages
+   *******************************************************************************/
+  public interface Message {}
+
+  public static final class CreateTicketStock implements Message {
+    public final int ticketId;
+    public final int quantity;
+    public final ActorRef<String> sender;
+
+    public CreateTicketStock(int ticketId, int quantity, ActorRef<String> sender) {
+      this.ticketId = ticketId;
+      this.quantity = quantity;
+      this.sender = sender;
+    }
+  }
+
+}
